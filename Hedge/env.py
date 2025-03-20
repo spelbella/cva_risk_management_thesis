36fadc926826afd163482f27648318feceb1a785
Isabelles git <isabelle.byman.email@gmail.com>
2025-03-20 22:14:43 +0100

    normalize

--- Hedge/env.py
+++ Hedge/env.py
@@ -25,6 +25,8 @@ class tradingEng(gym.Env):
         uppera = np.concatenate([1*np.ones(18)])
         self.action_space = gym.spaces.Box(low = lowera,high = uppera, dtype=np.float32)
 
+        self.rewards = []
+
     def reset(self, seed = None, options = None):
         self.tIDX = 0
         self.pthIDX = self.pthIDX + 1
@@ -61,8 +63,8 @@ class tradingEng(gym.Env):
                 ])
 
     def prices(self):
-        swa = [self.currpth.Swaptions[i][self.tIDX] for i in range(1,10)]
-        qs = [self.currpth.Q_s[i][self.tIDX] for i in range(1,10)]
+        swa = [self.currpth.Swaptions[i][self.tIDX] for i in range(0,9)] #ÄNDRA??
+        qs = [self.currpth.Q_s[i][self.tIDX] for i in range(0,9)] #ÄNDRA??
         return dict({"Swaption Price" : swa, "Q Price" : qs})  
     
     def PnL(self):
@@ -86,11 +88,14 @@ class tradingEng(gym.Env):
         swapts = action[0:9]
         Qs = action[9:18]
         return dict({"Swaption Position" : swapts, "Q Position" : Qs})
+    
+    def get_original_reward(self):
+        return self.rewards
 
     # The meat and potatoes
     def step(self, action):
         # Format action and try to avoid sideeffects
-        actionl = action.copy()/100000
+        actionl = action.copy() # /1000000
         if not isinstance(actionl, dict):
             actionl = self.vec_to_dict(actionl)
         
@@ -103,15 +108,16 @@ class tradingEng(gym.Env):
         
 
         # Observe the reward, which comes from the previous action
-        reward = -self.PnL()**2
+        reward = -self.PnL()**2# * 1e6
+        self.rewards.append(reward)
         info = self._get_info()
         observation = self._get_obs()
   
         # End the environment after we reach year 9
         terminated = bool(self.currpth.t_s[self.tIDX] > 9)
         truncated = False
-        if terminated:
-            self.reset()
+        # if terminated:
+        #     self.reset()
 
         return observation, reward, terminated, truncated, info
     
